a7765579050506ff9abb18131b8144b4e39747a7
Raashid Ansari <raash1d@users.noreply.github.com>
2018-05-22 11:34:41 -0400

    change libosgearth3-dev to libosgearth-dev

--- veins/omnetpp_install_cmds.sh
+++ veins/omnetpp_install_cmds.sh
@@ -1,10 +1,10 @@
 cd into omnetpp directory and then run these commands
 
-./setenv # set environment variables for omnetpp
+. ./setenv # set environment variables for omnetpp
 sudo apt-get install build-essential gcc g++ bison flex perl \\
      qt5-default tcl-dev tk-dev libxml2-dev zlib1g-dev default-jre \\
      doxygen graphviz libwebkitgtk-3.0-0 nemiver \\
-     libopenscenegraph-dev libosgearth3-dev libosgearth3 \\
+     libopenscenegraph-dev libosgearth-dev libosgearth3 \\
      libosgearthutil3 openscenegraph-plugin-osgearth
 ./configure
 make -j2
