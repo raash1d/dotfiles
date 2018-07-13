d4f3e097b1235aca8fcb36479015f4ea49694527
raash1d <mransari@onboardsecurity.com>
2018-07-13 16:54:00 -0400

    add dependencies to sumo install script

--- veins/sumo_install_cmds.sh
+++ veins/sumo_install_cmds.sh
@@ -1,4 +1,5 @@
 # cd into the sumo directory and these commands
+sudo apt install libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
 make -f Makefile.cvs # for plexe-sumo only
 ./configure --with-python --with-osg --with-ffmpeg
 make -j2
