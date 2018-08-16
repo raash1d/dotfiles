506dd270728325b1c18d7b4acf186666b35c233a
raash1d <mransari@onboardsecurity.com>
2018-08-16 09:19:22 -0400

    update sumo and omnetpp installations commands

--- veins/sumo_install_cmds.sh
+++ veins/sumo_install_cmds.sh
@@ -1,5 +1,6 @@
 # cd into the sumo directory and these commands
-sudo apt install libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
+sudo apt install libavcodec-dev libavformat-dev libavutil-dev libswscale-dev\
+  libxerces-c-dev libfox-1.6-dev
 make -f Makefile.cvs # for plexe-sumo only
 ./configure --with-python --with-osg --with-ffmpeg
 make -j2
