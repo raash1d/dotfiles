b5c2682c878d72b2a65a353792ee30ffec8a68ec
Raashid Ansari <raansari@mac0018506.silabs.com>
2024-03-25 16:16:12 -0400

    re-enable auto expand width in neo-tree to see file info when looking at long paths

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,9 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        auto_expand_width = true,
+      },
       filesystem = {
         filtered_items = {
           hide_dotfiles = false,
