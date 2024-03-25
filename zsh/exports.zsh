250d2c0745be60a995f334d62107c2d57a5362dd
Raashid Ansari <raansari@mac0018506.silabs.com>
2024-03-25 16:14:22 -0400

    move PATH export to exports.zsh

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,2 +1,5 @@
 # general exports used on every machine
 
+# export PATH="$HOME/bin:/usr/local/bin:$PATH"
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH:$RUST_PATH:$GO_PATH"
+
--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -6,9 +6,6 @@ RUST_PATH="$HOME/.cargo/bin"
 # add go toolchain to PATH
 GO_PATH="/usr/local/go/bin"
 
-# export PATH="$HOME/bin:/usr/local/bin:$PATH"
-export PATH="/opt/local/bin:/opt/local/sbin:$PATH:$RUST_PATH:$GO_PATH"
-
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
