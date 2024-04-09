9a7195341ed3790de6f6a81173c02d3445a049f4
Raashid Ansari <raashid.ansari@silabs.com>
2024-04-09 11:43:19 -0400

    completely remove conceal level for markdown files (and potentially for others)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- remove concealing information
+-- added the following specifically for markdown but may have effects on other files, which is fine
+vim.wo.conceallevel = 0
