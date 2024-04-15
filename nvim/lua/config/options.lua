8bcf94b31b65cbdda84e45df0263e76f719e08a2
Raashid Ansari <raashid.ansari@silabs.com>
2024-04-15 16:52:01 -0400

    add filetype detection config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,14 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.filetype.add({
+  filename = {
+    ["requirements.txt"] = "config",
+  },
+  pattern = {
+  },
+})
+
 -- remove concealing information
 -- added the following specifically for markdown but may have effects on other files, which is fine
 vim.wo.conceallevel = 0
