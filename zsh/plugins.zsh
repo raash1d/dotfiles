b4de38db57a5a757b6b69c91fc566a395cbb1f6e
Raashid Ansari <raashid.ansari@silabs.com>
2025-04-11 19:57:12 -0400

    create a plugin installer of sorts within plugins.zsh

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,14 +1,60 @@
-if [ ! -d $HOME/.zsh/zsh-autosuggestions/.git ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-fi
-source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+_zsh_plugin_usage() {
+  echo "Usage:"
+  echo "zsh_plugin <command> <plugin>"
+  echo "Parameters:"
+  echo "command: install, remove"
+  echo "plugin: e.g., lukechilds/zsh-nvm"
+  echo ""
+  echo "Examples:"
+  echo "zsh_plugin install lukechilds/zsh-nvm"
+  echo "zsh_plugin remove lukechilds/zsh-nvm"
+}
 
-if [ ! -d $HOME/.zsh/fast-syntax-highlighting/.git ]; then
-  git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.zsh/fast-syntax-highlighting
-fi
-source ~/.zsh/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+zsh_plugin() {
+  if [[ $# -gt 2 ]]; then
+    _zsh_plugin_usage
+  fi
 
-if [ ! -d $HOME/.zsh/zsh-nvm/.git ]; then
-  git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh/zsh-nvm
-fi
-source ~/.zsh/zsh-nvm/zsh-nvm.plugin.zsh
+  local cmd="$1"
+  local plugin="$2"
+
+  case "$cmd" in
+    "install")
+      _install_zsh_plugin $plugin
+      ;;
+    "remove")
+      _remove_zsh_plugin $plugin
+      ;;
+    *) echo default
+      echo "Invalid arguments"
+      _zsh_plugin_usage
+      ;;
+  esac
+  
+}
+
+INSTALL_DIR="$HOME/.zsh"
+
+_install_zsh_plugin() {
+  local plugin_dir="$INSTALL_DIR/$1"
+  if [ ! -d "$plugin_dir/.git" ]; then
+    git clone "https://github.com/$1.git" "$plugin_dir"
+  fi
+  for file in $plugin_dir/*.zsh; do
+    echo "sourcing $file"
+    [ -f "$file" ] && source "$file"
+  done
+}
+
+_remove_zsh_plugin() {
+  local plugin_dir="$INSTALL_DIR/$1"
+  if [ -d "$plugin_dir/.git" ]; then
+    rm -rf "$plugin_dir"
+    echo "removed $1 plugin"
+  fi
+}
+
+zsh_plugin install zsh-users/zsh-autosuggestions
+zsh_plugin install zsh-users/zsh-syntax-highlighting
+# zsh_plugin install zdharma-continuum/fast-syntax-highlighting
+zsh_plugin install lukechilds/zsh-nvm
