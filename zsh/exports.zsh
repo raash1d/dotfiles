19cd1feae5ae12c6807618c0bb6e02019622e3de
Raashid Ansari <raashid.ansari@silabs.com>
2025-04-11 03:07:21 -0400

    add homebrew path

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,13 +1,11 @@
 # general exports used on every machine
 
-# add rust toolchain to PATH
 RUST_PATH="$HOME/.cargo/bin"
-
-# add go toolchain to PATH
 GO_PATH="/usr/local/go/bin"
+HOMEBREW_PATH="/opt/homebrew/bin"
 
 export DOTFILES="$HOME/dotfiles"
 
 # export PATH="$HOME/bin:/usr/local/bin:$PATH"
-export PATH="/opt/local/bin:/opt/local/sbin:$PATH:$RUST_PATH:$GO_PATH"
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH:$RUST_PATH:$GO_PATH:$HOMEBREW_PATH"
 
