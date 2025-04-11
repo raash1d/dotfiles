73d6bc5626ac5c49297e220cca8ebccda4a71961
Raashid Ansari <raashid.ansari@silabs.com>
2025-04-11 03:17:37 -0400

    add homebrew bin path to PATH only on macOS

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,10 +2,13 @@
 
 RUST_PATH="$HOME/.cargo/bin"
 GO_PATH="/usr/local/go/bin"
-HOMEBREW_PATH="/opt/homebrew/bin"
-
 export DOTFILES="$HOME/dotfiles"
 
 # export PATH="$HOME/bin:/usr/local/bin:$PATH"
-export PATH="/opt/local/bin:/opt/local/sbin:$PATH:$RUST_PATH:$GO_PATH:$HOMEBREW_PATH"
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH:$RUST_PATH:$GO_PATH"
+
+if [[ $(uname -o) -eq "Darwin" ]]; then
+  HOMEBREW_PATH="/opt/homebrew/bin"
+  export PATH="$PATH::$HOMEBREW_PATH"
+fi
 
