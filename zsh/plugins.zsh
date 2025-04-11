c747dd1bf2cd645b6ed459336f5e09b5e68fd37f
Raashid Ansari <raashid.ansari@silabs.com>
2025-04-11 01:58:51 -0400

    test plugin directories based on their git directories

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,14 +1,14 @@
-if [ ! -d $HOME/.zsh/zsh-autosuggestions ]; then
+if [ ! -d $HOME/.zsh/zsh-autosuggestions/.git ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 fi
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
-if [ ! -d $HOME/.zsh/fast-syntax-highlighting ]; then
+if [ ! -d $HOME/.zsh/fast-syntax-highlighting/.git ]; then
   git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.zsh/fast-syntax-highlighting
 fi
 source ~/.zsh/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
 
-# if [ ! -d $HOME/.zsh/zsh-nvm ]; then
-#   git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh/zsh-nvm
-# fi
-# source ~/.zsh/zsh-nvm/zsh-nvm.plugin.zsh
+if [ ! -d $HOME/.zsh/zsh-nvm/.git ]; then
+  git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh/zsh-nvm
+fi
+source ~/.zsh/zsh-nvm/zsh-nvm.plugin.zsh
