1d01a405cdf6380264876424e32400155655f7a7
Raashid Ansari <raashid.ansari@silabs.com>
2025-05-21 20:03:47 -0400

    update venv-selector to regexp branch

--- nvim/lua/plugins/venv-selector.lua
+++ nvim/lua/plugins/venv-selector.lua
@@ -1,18 +1,20 @@
 return {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-    opts = {
-        -- Your options go here
-        -- name = "venv",
-        -- auto_refresh = false
-        path = "/Users/raansari/src/venvs/",
-        parents = 0,
-    },
-    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-    keys = {
-        -- Keymap to open VenvSelector to pick a venv.
-        { "<leader>vs", "<cmd>VenvSelect<cr>" },
-        -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-        { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
-    },
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap",
+    "mfussenegger/nvim-dap-python", --optional
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  },
+  lazy = false,
+  branch = "regexp", -- This is the regexp branch, use this for the new version
+  keys = {
+    { ",v", "<cmd>VenvSelect<cr>" },
+  },
+  ---@type venv-selector.Config
+  opts = {
+    -- Your settings go here
+    path = "/Users/raansari/src/venvs/",
+    parents = 0,
+  },
 }
