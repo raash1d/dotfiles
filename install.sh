19fda7ad8c0f09b0e366f5afc7108801e5cd4f95
Raashid Ansari <m.raashid.ansari@gmail.com>
2025-06-07 13:28:56 -0400

    formatting: format based on shfmt (4 -> 2 spaces)

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@
 
 SUDO=
 if [ "$(whoami)" != root ]; then
-    SUDO="sudo"
+  SUDO="sudo"
 else
-    read -n 1 -p "Do you really want to run as root? (y/[n]): " really_root
-    if [[ "$really_root" != y ]]; then
-        exit 0
-    fi
+  read -n 1 -p "Do you really want to run as root? (y/[n]): " really_root
+  if [[ "$really_root" != y ]]; then
+    exit 0
+  fi
 fi
 export SUDO
 
@@ -22,49 +22,49 @@ font_path="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Robo
 ################ Helper functions ################
 # usage: create_file_link <folder> <file> [link-name]
 create_file_link() {
-    echo -n "creating link for $2 ..."
-    if ! [ -f "$HOME/$2" ]; then
-        if [ "$#" -eq 3 ]; then
-            ln -s -f "$HOME/dotfiles/$1/$2" "$HOME/$3"
-        elif [ "$#" -eq 2 ]; then
-            ln -s -f "$HOME/dotfiles/$1/$2" "$HOME/$2"
-        fi
+  echo -n "creating link for $2 ..."
+  if ! [ -f "$HOME/$2" ]; then
+    if [ "$#" -eq 3 ]; then
+      ln -s -f "$HOME/dotfiles/$1/$2" "$HOME/$3"
+    elif [ "$#" -eq 2 ]; then
+      ln -s -f "$HOME/dotfiles/$1/$2" "$HOME/$2"
     fi
-    echo " done."
+  fi
+  echo " done."
 }
 
 create_folder_link() {
-    echo -n "creating link for folder $1 ..."
-    if ! [ -d "$HOME/$1" ]; then
-        ln -s -f "$HOME/dotfiles/$1" "$HOME/$1"
-    fi
-    echo " done."
+  echo -n "creating link for folder $1 ..."
+  if ! [ -d "$HOME/$1" ]; then
+    ln -s -f "$HOME/dotfiles/$1" "$HOME/$1"
+  fi
+  echo " done."
 }
 
 ############## git specific settings #############
 git_steps() {
-    # Check/Install git dependencies
+  # Check/Install git dependencies
 
-    # echo "Installing Meld diff viewer"
-    # case "$(uname)" in
-    # Darwin)
-    # 	brew tap homebrew/cask
-    # 	brew cask install meld
-    # 	;;
-    # Linux)
-    # 	# lib/install meld
-    # 	;;
-    # esac
+  # echo "Installing Meld diff viewer"
+  # case "$(uname)" in
+  # Darwin)
+  # 	brew tap homebrew/cask
+  # 	brew cask install meld
+  # 	;;
+  # Linux)
+  # 	# lib/install meld
+  # 	;;
+  # esac
 
-    echo "Installing Git"
-    lib/install git
+  echo "Installing Git"
+  lib/install git
 
-    # Create git symlinks
-    echo "Creating git symlinks"
-    cp "$HOME/dotfiles/git/.gitconfig" "$HOME/"
+  # Create git symlinks
+  echo "Creating git symlinks"
+  cp "$HOME/dotfiles/git/.gitconfig" "$HOME/"
 
-    # echo "Cloning Git submodules"
-    # git submodule update --init --recursive
+  # echo "Cloning Git submodules"
+  # git submodule update --init --recursive
 }
 ##################################################
 
@@ -98,70 +98,70 @@ git_steps() {
 
 ############### zsh specific steps ###############
 zsh_steps() {
-    echo "Installing zsh"
-    lib/install zsh
+  echo "Installing zsh"
+  lib/install zsh
 
-    echo "Cleaning up previous zsh config files"
-    [ -f ~/.zshrc ] && rm ~/.zshrc
+  echo "Cleaning up previous zsh config files"
+  [ -f ~/.zshrc ] && rm ~/.zshrc
 
-    create_file_link zsh .zshrc
+  create_file_link zsh .zshrc
 
-    echo "Setup zsh as default shell"
-    chsh -s "$(command -v zsh)"
+  echo "Setup zsh as default shell"
+  chsh -s "$(command -v zsh)"
 }
 ##################################################
 
 ############## tmux specific steps ###############
 tmux_steps() {
-    # TODO:Install tmux dependencies
-    # libevent-dev libncurses-dev yacc
-    # TODO: Use the following link to install latest
-    # https://github.com/tmux/tmux/releases
-    # TODO: make this installable cross-platform
-    echo "Installing tmux"
-    lib/install tmux
+  # TODO:Install tmux dependencies
+  # libevent-dev libncurses-dev yacc
+  # TODO: Use the following link to install latest
+  # https://github.com/tmux/tmux/releases
+  # TODO: make this installable cross-platform
+  echo "Installing tmux"
+  lib/install tmux
 
-    # Create symlinks for tmux
-    create_file_link tmux tmux.conf .tmux.conf
+  # Create symlinks for tmux
+  create_file_link tmux tmux.conf .tmux.conf
 
-    echo "Installing tmux clipboard support and creating links to local tmux configs"
-    case "$(uname)" in
-    Darwin)
-        lib/install reattach-to-user-namespace
-        ;;
-    Linux)
-        lib/install xsel
-        ;;
-    esac
+  echo "Installing tmux clipboard support and creating links to local tmux configs"
+  case "$(uname)" in
+  Darwin)
+    lib/install reattach-to-user-namespace
+    ;;
+  Linux)
+    lib/install xsel
+    ;;
+  esac
 }
 ##################################################
 
 ############### linux specific steps ###############
 install_fonts() {
-    echo "Installing $font from nerd-fonts"
-    font_name="Roboto Mono Nerd Font Complete.ttf"
-    #(
-    #cd /tmp
-    # clone nerd-fonts repo
-    #if [ ! -d nerd-fonts ]; then
-    #git clone https://github.com/ryanoasis/nerd-fonts.git --depth=1
-    #fi
+  echo "Installing $font from nerd-fonts"
+  font_name="Roboto Mono Nerd Font Complete.ttf"
+  #(
+  #cd /tmp
+  # clone nerd-fonts repo
+  #if [ ! -d nerd-fonts ]; then
+  #git clone https://github.com/ryanoasis/nerd-fonts.git --depth=1
+  #fi
 
-    # install
-    #cd nerd-fonts
-    #./install.sh "$font"
-    #)
+  # install
+  #cd nerd-fonts
+  #./install.sh "$font"
+  #)
 
-    case "$(uname)" in
-    Linux)
-        mkdir -p ~/.local/share/fonts
-        curl -fLo "$HOME/.local/share/fonts/$font_name" "$font_path"
-        ;;
-    Darwin)
-        curl -fLo "/Users/$(whoami)/Library/Fonts/$font_name" "$font_path"
-        ;;
-    esac
-    fc-cache -vf
+  case "$(uname)" in
+  Linux)
+    mkdir -p ~/.local/share/fonts
+    curl -fLo "$HOME/.local/share/fonts/$font_name" "$font_path"
+    ;;
+  Darwin)
+    curl -fLo "/Users/$(whoami)/Library/Fonts/$font_name" "$font_path"
+    ;;
+  esac
+  fc-cache -vf
 }
 ##################################################
 
@@ -172,223 +172,223 @@ install_fonts() {
 ##################################################
 
 post_install_steps() {
-    echo "Please configure your git:"
-    echo "Enter your name: "
-    read -r gitusername
-    echo "Enter your email: "
-    read -r gituseremail
+  echo "Please configure your git:"
+  echo "Enter your name: "
+  read -r gitusername
+  echo "Enter your email: "
+  read -r gituseremail
 
-    git config --global user.name "$gitusername"
-    git config --global user.email "$gituseremail"
+  git config --global user.name "$gitusername"
+  git config --global user.email "$gituseremail"
 
-    if [[ "$(uname)" == "Darwin" ]]; then
-        # make clang tools available to cli
-        ln -s -f "$(brew --prefix llvm)/bin/clang-format" "/usr/local/bin/clang-format"
-        ln -s -f "$(brew --prefix llvm)/bin/clang-tidy" "/usr/local/bin/clang-tidy"
-    fi
+  if [[ "$(uname)" == "Darwin" ]]; then
+    # make clang tools available to cli
+    ln -s -f "$(brew --prefix llvm)/bin/clang-format" "/usr/local/bin/clang-format"
+    ln -s -f "$(brew --prefix llvm)/bin/clang-tidy" "/usr/local/bin/clang-tidy"
+  fi
 }
 
 pre_install_steps() {
-    echo "Answer the following before the automated installation starts."
-    echo "Enter golang toolchain tarball (.tar.gz) link (https://golang.org/dl): "
-    read -r golangURL
+  echo "Answer the following before the automated installation starts."
+  echo "Enter golang toolchain tarball (.tar.gz) link (https://golang.org/dl): "
+  read -r golangURL
 
-    if [[ "$(uname)" == "Linux" ]]; then
-      if [[ ("$(lsb_release -si)" == "elementary") || ("$(lsb_release -si)" == "Ubuntu") || ("$(lsb_release -si)" == "Debian") ]]; then
-        echo "Enter link to download shfmt (shellformat) util (https://github.com/mvdan/sh/releases):"
-        read -r shfmtURL
-      fi
+  if [[ "$(uname)" == "Linux" ]]; then
+    if [[ ("$(lsb_release -si)" == "elementary") || ("$(lsb_release -si)" == "Ubuntu") || ("$(lsb_release -si)" == "Debian") ]]; then
+      echo "Enter link to download shfmt (shellformat) util (https://github.com/mvdan/sh/releases):"
+      read -r shfmtURL
+    fi
+  fi
+
+  echo "Enter lazygit tarball (.tar.gz) link (https://github.com/jesseduffield/lazygit/releases): "
+  read -r lazygitURL
+
+  case "$(uname)" in
+  Darwin)
+    # install Homebrew on macOS
+    if [ -x "$(command -v brew)" ]; then
+      echo "Homebrew already installed"
+    else
+      echo "Installing Homebrew"
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     fi
 
-    echo "Enter lazygit tarball (.tar.gz) link (https://github.com/jesseduffield/lazygit/releases): "
-    read -r lazygitURL
+    echo "Installing Python toolchain"
+    lib/install python
+    ;;
+  Linux)
+    echo "Installing Python toolchain"
+    $SUDO apt install python3 python3-pip -y
+    # echo "Enter Python toolchain tarball (.tar.xz) link from https://www.python.org/downloads/: "
+    # read pythonURL
 
-    case "$(uname)" in
-    Darwin)
-        # install Homebrew on macOS
-        if [ -x "$(command -v brew)" ]; then
-            echo "Homebrew already installed"
-        else
-            echo "Installing Homebrew"
-            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-        fi
+    # #install python toolchain
+    # cd /tmp
+    # echo "Downloading Python toolchain"
+    # curl -LO --progress-bar "$pythonURL"
+    # py_tarball=${pythonURL##*/} # get tarball name only
+    # echo "Extracting Python toolchain"
+    # tar xf "$py_tarball"
+    # cd "${py_tarball%.*.*}" # get folder name only
+    # echo "Installing Python toolchain"
+    # ./configure --enable-optimizations
+    # $SUDO make -j2
+    # $SUDO make install
+    # cd ..
+    # cd ~/dotfiles
+    ;;
+  esac
 
-        echo "Installing Python toolchain"
-        lib/install python
-        ;;
-    Linux)
-        echo "Installing Python toolchain"
-        $SUDO apt install python3 python3-pip -y
-        # echo "Enter Python toolchain tarball (.tar.xz) link from https://www.python.org/downloads/: "
-        # read pythonURL
+  # rust toolchain
+  PATH="$PATH:/$(whoami)/.cargo/bin"
+  export PATH
+  if [ -x "$(command -v rustc)" ]; then
+    echo "Rust toolchain already installed"
+  else
+    echo "Installing Rust toolchain"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  fi
 
-        # #install python toolchain
-        # cd /tmp
-        # echo "Downloading Python toolchain"
-        # curl -LO --progress-bar "$pythonURL"
-        # py_tarball=${pythonURL##*/} # get tarball name only
-        # echo "Extracting Python toolchain"
-        # tar xf "$py_tarball"
-        # cd "${py_tarball%.*.*}" # get folder name only
-        # echo "Installing Python toolchain"
-        # ./configure --enable-optimizations
-        # $SUDO make -j2
-        # $SUDO make install
-        # cd ..
-        # cd ~/dotfiles
-        ;;
-    esac
+  # golang toolchain
+  if [ -x "$(command -v go)" ]; then
+    echo "Go toolchain already installed"
+  else
+    (
+      cd /tmp || return
+      echo "Downloading Go toolchain"
+      curl -LO --progress-bar "$golangURL"
+      echo "Installing Go toolchain"
+      $SUDO tar -C /usr/local -xzf "${golangURL##*/}" # get name of tarball only
+    )
 
-    # rust toolchain
-    PATH="$PATH:/$(whoami)/.cargo/bin"
-    export PATH
-    if [ -x "$(command -v rustc)" ]; then
-        echo "Rust toolchain already installed"
-    else
-        echo "Installing Rust toolchain"
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    # Temporarily export go path in PATH
+    export PATH="$PATH:/usr/local/go/bin"
+  fi
+
+  # lazygit
+  if [ -x "$(command -v lazygit)" ]; then
+    echo "Go toolchain already installed"
+  else
+    (
+      cd /tmp || return
+      echo "Downloading lazygit"
+      curl -LO --progress-bar "$lazygitURL"
+      echo "Installing lazygit"
+      $SUDO tar -C /usr/local/bin -xzf "${lazygitURL##*/}" # get name of tarball only
+    )
+  fi
+
+  # tools on linux
+  if [[ "$(uname)" == "Linux" ]]; then
+    if [[ ("$(lsb_release -si)" == "elementary") || ("$(lsb_release -si)" == "Ubuntu") || ("$(lsb_release -si)" == "Debian") ]]; then
+      echo "Installing Linux tools"
+      lib/install build-essential # software-properties-common clang clang-tools-8 clang-tidy clang-format
+      # echo "Updating clangd alternatives"
+      # $SUDO update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-8 100
     fi
 
-    # golang toolchain
-    if [ -x "$(command -v go)" ]; then
-        echo "Go toolchain already installed"
-    else
-        (
-            cd /tmp || return
-            echo "Downloading Go toolchain"
-            curl -LO --progress-bar "$golangURL"
-            echo "Installing Go toolchain"
-            $SUDO tar -C /usr/local -xzf "${golangURL##*/}" # get name of tarball only
-        )
-
-        # Temporarily export go path in PATH
-        export PATH="$PATH:/usr/local/go/bin"
+    # elementary OS steps
+    if [[ "$(lsb_release -si)" == "elementary" ]]; then
+      echo "Adding elementary-tweaks ppa"
+      $SUDO add-apt-repository ppa:philip.scott/elementary-tweaks -y
+      lib/install elementary-tweaks
     fi
+  fi
 
-    # lazygit
-    if [ -x "$(command -v lazygit)" ]; then
-        echo "Go toolchain already installed"
-    else
+  # C/C++ toolchain
+  # echo "Installing C/C++ toolchain"
+  # lib/install llvm cmake uncrustify
+
+  # node/javascript/typescript toolchain
+  # echo "Installing Node/JavaScript/TypeScript toolchain"
+  # lib/install libssl1.0-dev nodejs-dev node-gyp npm
+  # $SUDO npm install -g typescript
+
+  # generic utilities
+  lib/install curl shellcheck direnv
+  echo "Install shellformat (shfmt)"
+  if [[ "$(uname)" == "Darwin" ]]; then
+    lib/install shfmt
+  elif [[ "$(uname)" == "Linux" ]]; then
+    if [[ ("$(lsb_release -si)" == "elementary") || ("$(lsb_release -si)" == "Ubuntu") || ("$(lsb_release -si)" == "Debian") ]]; then
+      if [ -x "$(command -v shfmt)" ]; then
+        echo "shfmt already installed"
+      else
         (
           cd /tmp || return
-          echo "Downloading lazygit"
-          curl -LO --progress-bar "$lazygitURL"
-          echo "Installing lazygit"
-          $SUDO tar -C /usr/local/bin -xzf "${lazygitURL##*/}" # get name of tarball only
+          echo "Downloading shfmt utility"
+          curl -L --progress-bar -o shfmt "$shfmtURL"
+          chmod +x shfmt
+          echo "Installing shfmt"
+          $SUDO mv shfmt /usr/local/bin/
         )
+      fi
     fi
+  fi
 
-    # tools on linux
-    if [[ "$(uname)" == "Linux" ]]; then
-        if [[ ("$(lsb_release -si)" == "elementary") || ("$(lsb_release -si)" == "Ubuntu") || ("$(lsb_release -si)" == "Debian") ]]; then
-            echo "Installing Linux tools"
-            lib/install build-essential # software-properties-common clang clang-tools-8 clang-tidy clang-format
-            # echo "Updating clangd alternatives"
-            # $SUDO update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-8 100
-        fi
+  # install rust-based utilities
+  # dependency for alacritty
+  lib/install libfontconfig1-dev
+  cargo install starship bat eza fd-find ripgrep tealdeer alacritty zoxide git-delta --locked
+  echo "Loading tldr cache"
+  tldr --update
+  ln -sf "$HOME/dotfiles/alacritty" "$HOME/.config/"
+  ln -sf "$HOME/dotfiles/lazygit" "$HOME/.config/"
+  ln -sf "$HOME/dotfiles/starship/starship.toml" "$HOME/.config/"
 
-        # elementary OS steps
-        if [[ "$(lsb_release -si)" == "elementary" ]]; then
-            echo "Adding elementary-tweaks ppa"
-            $SUDO add-apt-repository ppa:philip.scott/elementary-tweaks -y
-            lib/install elementary-tweaks
-        fi
-    fi
-
-    # C/C++ toolchain
-    # echo "Installing C/C++ toolchain"
-    # lib/install llvm cmake uncrustify
-
-    # node/javascript/typescript toolchain
-    # echo "Installing Node/JavaScript/TypeScript toolchain"
-    # lib/install libssl1.0-dev nodejs-dev node-gyp npm
-    # $SUDO npm install -g typescript
-
-    # generic utilities
-    lib/install curl shellcheck direnv
-    echo "Install shellformat (shfmt)"
-    if [[ "$(uname)" == "Darwin" ]]; then
-        lib/install shfmt
-    elif [[ "$(uname)" == "Linux" ]]; then
-        if [[ ("$(lsb_release -si)" == "elementary") || ("$(lsb_release -si)" == "Ubuntu") || ("$(lsb_release -si)" == "Debian") ]]; then
-            if [ -x "$(command -v shfmt)" ]; then
-                echo "shfmt already installed"
-            else
-                (
-                    cd /tmp || return
-                    echo "Downloading shfmt utility"
-                    curl -L --progress-bar -o shfmt "$shfmtURL"
-                    chmod +x shfmt
-                    echo "Installing shfmt"
-                    $SUDO mv shfmt /usr/local/bin/
-                )
-            fi
-        fi
-    fi
-
-    # install rust-based utilities
-    # dependency for alacritty
-    lib/install libfontconfig1-dev
-    cargo install starship bat eza fd-find ripgrep tealdeer alacritty zoxide git-delta --locked
-    echo "Loading tldr cache"
-    tldr --update
-    ln -sf "$HOME/dotfiles/alacritty" "$HOME/.config/"
-    ln -sf "$HOME/dotfiles/lazygit" "$HOME/.config/"
-    ln -sf "$HOME/dotfiles/starship/starship.toml" "$HOME/.config/"
-
-    #install fzf (fuzzy file finder)
-    if [ -x "$(command -v fzf)" ]; then
-        echo "fzf already installed"
-    else
-        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-        ~/.fzf/install
-    fi
+  #install fzf (fuzzy file finder)
+  if [ -x "$(command -v fzf)" ]; then
+    echo "fzf already installed"
+  else
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+  fi
 }
 
 docker_steps() {
-    if [[ "$(uname)" == "Linux" ]]; then
-        if [ -x "$(command -v docker)" ]; then
-            echo "docker already installed"
-        else
-            echo "Install docker using instructions at https://docs.docker.com/engine/install/"
-        fi
+  if [[ "$(uname)" == "Linux" ]]; then
+    if [ -x "$(command -v docker)" ]; then
+      echo "docker already installed"
+    else
+      echo "Install docker using instructions at https://docs.docker.com/engine/install/"
     fi
+  fi
 }
 
 ############### steps to perform manually ###############
 manual_steps() {
-    step="
+  step="
         To complete installation, follow below steps
         1. Change your font to $font for your terminal"
-    if [[ "$(uname)" == "Darwin" ]]; then
-        step=$step"
+  if [[ "$(uname)" == "Darwin" ]]; then
+    step=$step"
         3. After installing VSCode, run \"defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false\""
-    fi
+  fi
 
-    echo "$step"
+  echo "$step"
 }
 ##################################################
 
 # check if OS is supported
 case "$(uname)" in
 Darwin)
-    echo "You're on macOS, IT'S SUPPORTED! :)"
-    ;;
+  echo "You're on macOS, IT'S SUPPORTED! :)"
+  ;;
 Linux)
-    case "$(lsb_release -si)" in
-    Ubuntu | elementary | Debian)
-        echo "You're on an Ubuntu-based distro, IT'S SUPPORTED! :)"
-        ;;
-    *)
-        echo "Unfortunately your linux distro is not supported yet. :("
-        exit
-        ;;
-    esac
+  case "$(lsb_release -si)" in
+  Ubuntu | elementary | Debian)
+    echo "You're on an Ubuntu-based distro, IT'S SUPPORTED! :)"
     ;;
-*)
-    echo "Unfortunately your OS is not supported yet. :("
+  *)
+    echo "Unfortunately your linux distro is not supported yet. :("
     exit
     ;;
+  esac
+  ;;
+*)
+  echo "Unfortunately your OS is not supported yet. :("
+  exit
+  ;;
 esac
 
 pre_install_steps
